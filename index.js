b965ef118417f9d2d5e5addf70f4d0722657378c
Anju Sebastian <christeenaanju@gmail.com>
2025-11-20 11:32:20 +0530

    upd

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: "https://budgettrk.netlify.app",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
